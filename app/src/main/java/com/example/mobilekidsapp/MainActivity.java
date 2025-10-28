bed2a1a99199563573e525e26868692b2d96e0ae
MarvinFuentes <122904330+MarvinFuentes@users.noreply.github.com>
2025-10-28 13:42:35 -0400

    10-28-work

--- app/src/main/java/com/example/mobilekidsapp/MainActivity.java
+++ app/src/main/java/com/example/mobilekidsapp/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.mobilekidsapp;
 
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +12,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
+
+    TextToSpeech tts;
+    ImageButton txtToSpeechBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +29,46 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        txtToSpeechBtn = findViewById(R.id.speechBtn);
+
+        Button newStuBtn = (Button) findViewById(R.id.newStudentBtn);
+        newStuBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // GO TO new student page
+            }
+        });
+
+        Button existingStuBtn = (Button) findViewById(R.id.ExistingStudentBtn);
+        existingStuBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Go to existing student page
+            }
+        });
+
+        tts = new TextToSpeech(this, status -> {
+            if(status == TextToSpeech.SUCCESS){
+                tts.setLanguage(Locale.ENGLISH);
+            }
+        });
+
+        txtToSpeechBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String txtToSpeech = getString(R.string.main_page_speech);
+                tts.speak(txtToSpeech, TextToSpeech.QUEUE_FLUSH, null, null);
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        if(tts != null){
+            tts.stop();
+            tts.shutdown();
+        }
+        super.onDestroy();
     }
 }
